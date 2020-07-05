cc8a5545b05916157eafa6f206d92aff9f498925
ottojulian <j.ottomano@gmail.com>
2020-07-05 18:52:15 -0300

    modificacion de portada.js y portada.css. Se agregó background img, un título y un párrafo

--- src/portada/Portada.js
+++ src/portada/Portada.js
@@ -5,22 +5,15 @@ import './Portada.css';
 function Portada() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <h1>TÍTULO</h1>
+        <p>Los sitios web usan rastreadores para recopilar datos sobre el comportamiento de exploración. Los rastreadores recopilan datos sobre cómo interactúas con un sitio, como el contenido en el que haces clic. También habilitan la funcionalidad en algunos sitios web, como las secciones de comentarios, los botones para compartir artículos con las redes sociales y los anuncios personalizados.
+
+        Algunos rastreadores recopilan datos sobre el usuario en varios sitios. Por ejemplo, un rastreador podría realizar un seguimiento cuando vayas a tu sitio de compras y de noticias favoritos. Esta es la razón por la que, en ocasiones, puede parecer que un anuncio te está siguiendo por la web.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
-  );
+    );
 }
 
 export default Portada;
