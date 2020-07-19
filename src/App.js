996508677c1396022657c1cd8d973eed4f9f78b8
Damián Silvani <munshkr@gmail.com>
2020-07-19 13:21:35 -0300

    Tidy files (2 spaces)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 import './App.css'
 import Portada from './portada/Portada'
 
-function App () {
-    return(
-        <div className="App">
-            <Portada />
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <Portada />
+    </div>
+  )
 }
 
 export default App;
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 const theme = {
-    fonts: {
-        light: 'Open Sans Light',
-        regular: 'Open Sans Regular',
-        semibold: 'Open Sans Semibold',
-        extrabold: 'Open Sans ExtraBold',
-        italic: 'Open Sans Italic',
-    }
-} 
+  fonts: {
+    light: 'Open Sans Light',
+    regular: 'Open Sans Regular',
+    semibold: 'Open Sans Semibold',
+    extrabold: 'Open Sans ExtraBold',
+    italic: 'Open Sans Italic',
+  }
+}
 
 export default theme
