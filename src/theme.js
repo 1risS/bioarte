63f25924e045192fb938e122824f8e8eeab1e3a8
Damián Silvani <munshkr@gmail.com>
2020-07-19 14:51:05 -0300

    Define colors from style guide in theme

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,12 @@
 const theme = {
-  fonts: {
-    light: 'Open Sans Light',
-    regular: 'Open Sans Regular',
-    semibold: 'Open Sans Semibold',
-    extrabold: 'Open Sans ExtraBold',
-    italic: 'Open Sans Italic',
+  colors: {
+    bg: '#fffff',
+    gray1: '#e7e8e6',
+    gray2: '#d6d7d5',
+    green1: '#dbe6d4',
+    yellow1: '#FFF2CC',
+    oranges: ['#EC6B42', '#DF5016', '#AC3E17'],
+    greens: ['#93BB66', '#508F44', '#005A32'],
   }
 }
 
