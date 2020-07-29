b8bd10b9f3da8fc8863c36c48c2e8aef584dfbf4
Damián Silvani <munshkr@gmail.com>
2020-07-29 18:04:42 -0300

    Fix Link import on Agenda2

--- src/components/Agenda2.js
+++ src/components/Agenda2.js
@@ -1,4 +1,5 @@
-import React, { useState, Link } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../images/logo192.png";
 
