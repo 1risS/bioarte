45c4874bd0085e31ec93c0987c663bfe9355fe6c
Iris Saladino <irissaladino@Iriss-MacBook-Pro.local>
2020-10-19 15:41:30 -0300

    ajustes estilo header Academia

--- src/pages/academia.js
+++ src/pages/academia.js
@@ -106,7 +106,7 @@ const BannerHubs = styled.img`
   height: 6rem;
   width: 6rem;
   align-self: flex-end;
-  margin-bottom: 3rem;
+  margin: 15rem 3rem 2rem 2rem; ;
 `
 
 const AgendaContainer = styled.div`
