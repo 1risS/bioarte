854d5dd943fa16660a8a7cf63b57e015fc4ec0cd
Damián Silvani <munshkr@gmail.com>
2021-03-17 21:34:56 -0300

    Use a square image for manifest image

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `static/images/BIOSlogo.png`, // This path is relative to the root of the site.
+        icon: `static/images/BIOSlogo-square.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`,
