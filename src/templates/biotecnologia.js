400d138d87439e40ecc79fcc95cf2b8a3222e474
Damián Silvani <munshkr@gmail.com>
2021-03-21 14:23:25 -0300

    Remove unused components on BiotecnologiaPageTemplate

--- src/templates/biotecnologia.js
+++ src/templates/biotecnologia.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Chat from "../components/chat"
-import logoFestival from "../images/Academia/logo libélula.png"
 import fondoHeader from "../../static/images/carousel/Banner_Exhibition_0.png"
 import libelula from "../../static/images/carousel/Dragonfly.png"
 import fondoObra from "../../static/images/artists/anaLauraCantera/obra_nueva.jpg"
@@ -17,6 +15,7 @@ import Footer from "../components/footer"
 const DAContainer = styled.div`
   background-color: ${props => props.theme.colors.bg1};
 `
+
 const DAHeaderContainer = styled.div`
   display: flex;
   width: auto;
@@ -29,6 +28,7 @@ const DAHeaderContainer = styled.div`
   background-repeat: no-repeat;
   margin-bottom: 7rem;
 `
+
 const NavBar = styled.div`
   display: flex;
   flex-direction: row;
@@ -36,86 +36,14 @@ const NavBar = styled.div`
   margin-top: 3rem;
   margin-bottom: -2rem;
 `
+
 const LogoContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
   width: 30%;
 `
-const Logo = styled.img`
-  height: 3rem;
-  width: 3rem;
-  margin-right: 3rem;
-  align-self: center;
-`
-const Placeholder1 = styled.img`
-  height: 3rem;
-  width: 3rem;
-  align-self: center;
-`
-const TimerNavegacionContainer = styled.div``
 
-const TimerNavegacion = styled.div`
-  max-width: 150px;
-  width: 100%;
-
-  text-align: center;
-  font-family: ${props => props.theme.fontFamily};
-  font-weight: ${props => props.theme.fontWeights.light};
-  color: #fff;
-
-  display: flex;
-`
-
-const TiempoContainer = styled.div`
-  margin-left: 1em;
-  margin-right: 1em;
-`
-
-const Horas = styled.div`
-  border-radius: 20px;
-  background-color: rgba(0, 0, 0, 0.5);
-  font-size: 1.5rem;
-  font-weight: bold;
-  padding: 0 0.2em 0 0.2em;
-`
-const Min = styled.div`
-  border-radius: 20px;
-  background-color: rgba(0, 0, 0, 0.5);
-  font-size: 1.5rem;
-  font-weight: bold;
-  padding: 0 0.2em 0 0.2em;
-`
-
-const Seg = styled.div`
-  border-radius: 20px;
-  background-color: rgba(0, 0, 0, 0.5);
-  font-size: 1.5rem;
-  font-weight: bold;
-  padding: 0 0.2em 0 0.2em;
-`
-
-const CantidadVisitantesContainer = styled.div`
-  max-width: 150px;
-  width: 100%;
-  text-align: center;
-  font-family: ${props => props.theme.fontFamily};
-  font-weight: ${props => props.theme.fontWeights.light};
-  color: #fff;
-  margin-left: 10em;
-`
-
-const TextoContadores = styled.div`
-  font-weight: lighter;
-  font-size: 1em;
-`
-
-const CantidadVisitantes = styled.div`
-  border-radius: 20px;
-  background-color: rgba(0, 0, 0, 0.5);
-  font-size: 1.5rem;
-  font-weight: bold;
-`
 const Texto = styled.div`
   font-family: ${props => props.theme.fontFamily};
   font-size: 14rem;
@@ -127,10 +55,12 @@ const Texto = styled.div`
   padding-top: 25vh;
   /*line-height: initial;*/
 `
+
 const DAMain = styled.div`
   display: flex;
   flex-direction: column;
 `
+
 const BioArtistaContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -172,6 +102,7 @@ const BioTexto = styled.div`
   margin-top: 1em;
   padding: 32.25pt 24pt 32.25pt 24pt;
 `
+
 const FotoObraContainer = styled.div`
   background-image: url(${fondoObra});
   background-size: 100%;
@@ -188,30 +119,9 @@ const FotoObraContainer = styled.div`
   }
 `
 
-const NombreObra = styled.div`
-  font-size: 2em;
-  text-align: center;
-  color: #fff;
-  background-color: RGBA(0, 0, 0, 0.5);
-  width: 600px;
-  height: auto;
-  margin: 15em auto 0 auto;
-  padding: 1em;
-  border-radius: 15px;
-  &:hover {
-    opacity: 0.8;
-    cursor: pointer;
-  }
-`
 const Parrafo = styled.div`
   padding-top: 1em;
   font-size: 16pt;
-`
-const ParrafoFormacion = styled.div`
-  padding-top: 1em;
-  font-size: 16pt;
-  font-style: italic;
-  text-decoration: underline;
 `
 
 const Titulo = styled.div`
@@ -231,49 +141,15 @@ const LikeLikeCont = styled.div`
 
 const Like = styled.img``
 
-const CVCont = styled.div`
-  margin-left: 10rem;
-  align-self: flex-end;
-  margin: 0rem 0rem 3rem 0rem;
-  &:hover {
-    cursor: pointer;
-  }
-`
-
-const CV = styled.div`
-  width: 49px;
-  height: 49px;
-  background-color: #508f44;
-  border-radius: 50px;
-  display: flex;
-  justify-content: center;
-
-  &:hover {
-    background-color: #bed6a3;
-  }
-`
-
-const CVText = styled.div`
-  color: #fff;
-  font-size: 1.5em;
-  padding-top: 0.25em;
-`
-
 const TituloBio = styled.div`
   display: flex;
   width: 100%;
   flex-direction: row;
   justify-content: space-between;
 `
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: white;
-  font-weight: 200;
-`
 
-// ponemos una cookie para saber si le usuarix pasó por acá
 
-const ArtistPageTemplate = ({ pageContext }) => {
+const BiotecnologiaPageTemplate = ({ pageContext }) => {
   const { nombre, descripcion, secciones } = pageContext
 
   const [seccion, setSeccion] = useState(0)
@@ -343,17 +219,4 @@ const ArtistPageTemplate = ({ pageContext }) => {
   )
 }
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       html
-//       frontmatter {
-//         nombre
-//         formacion
-//         bio
-//       }
-//     }
-//   }
-// `
-
-export default ArtistPageTemplate
+export default BiotecnologiaPageTemplate
