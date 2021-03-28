d06611d07aeaafa04ef4032ec105663a5bb32b04
Iris Saladino <irissaladino@Iriss-MacBook-Pro.local>
2021-03-28 20:32:10 -0300

    footer con + logos

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,27 +2,61 @@ import React from "react"
 import styled from "styled-components"
 import logo1 from "../../static/images/BIOSlogo.png"
 
-const FooterCont = styled.div`
+const LogosCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  max-height: 20rem;
+  background-color: #272d28;
+`
+
+const FirstRow = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
   width: 100vw;
-  background-color: #272d28;
+`
+
+const SecRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-end;
+  width: 100vw;
+  height: 3rem;
+  margin-bottom: 1rem;
+  margin-top: 3rem;
 `
 
 const Logo = styled.img`
-  margin: 4rem;
+  margin: 3rem;
+`
+
+const Logo2 = styled.img`
+  height: 50px;
+  width: 50px;
+  margin: 2rem;
 `
 
 const Footer = () => {
   return (
-    <FooterCont>
-      <Logo src={logo1}></Logo>
-      <Logo src={logo1}></Logo>
-      <Logo src={logo1}></Logo>
-      <Logo src={logo1}></Logo>
-    </FooterCont>
+    <LogosCont>
+      <FirstRow>
+        <Logo src={logo1}></Logo>
+        <Logo src={logo1}></Logo>
+        <Logo src={logo1}></Logo>
+        <Logo src={logo1}></Logo>
+      </FirstRow>
+      <SecRow>
+        <Logo2 src={logo1}></Logo2>
+        <Logo2 src={logo1}></Logo2>
+        <Logo2 src={logo1}></Logo2>
+        <Logo2 src={logo1}></Logo2>
+        <Logo2 src={logo1}></Logo2>
+        <Logo2 src={logo1}></Logo2>
+      </SecRow>
+    </LogosCont>
   )
 }
 
