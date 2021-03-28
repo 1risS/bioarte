3794eb84f6a8561a984d9fcba031f118f1812c5e
Damián Silvani <munshkr@gmail.com>
2021-03-28 14:25:19 -0300

    Revert image crop

--- script/crop_carousel_image.py
+++ script/crop_carousel_image.py
@@ -16,7 +16,6 @@ PATTERNS = [
 ]
 SKIP_PATTERNS = [
     'artists/*/main.jpg',
-    'artists/*/profile.jpg',
     'biotecnologia/*/main.jpg',
     'biotecnologia/logoEmpresas/*',
     'biotecnologia/iconos/*',
