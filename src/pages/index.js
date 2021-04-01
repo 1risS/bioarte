364ff8c72567b02343d7ea5b43211c30de9ce3fc
Iris Saladino <irissaladino@Iriss-MacBook-Pro.local>
2021-04-01 11:02:32 -0300

    welcome info actual

--- src/pages/index.js
+++ src/pages/index.js
@@ -200,16 +200,19 @@ const IndexPage = () => {
           <DivIzq>
             <LightGuide top="40px" right="-120px" />
             <Block>
-              <Title>¿Quienes lo integran?</Title>
+              <Title>¿Qué es Proyecto Bios</Title>
               <Desc>
-                <br></br> Producción y coordinación general: Objeto a <br></br>
-                Diseño: Valeria Scalise <br></br>
-                Experiencia web: Iris Saladino (Programación) y Laura Palavecino
-                (Arte) <br></br>
-                Área Academia: Guillermo Winnicki <br></br>
-                Área Bioarte: Natalia Matewecki, Lucia Stubrin, Objeto a
                 <br></br>
-                Experiencia xxx: Marie Claire Flanagan
+                Proyecto Bios pretende conformar un círculo virtuoso entre Arte,
+                Academia y Ciencia para acompañar, difundir y divulgar los
+                avances de la Biotecnología en el país, visibilizar y potenciar
+                los trabajos de artistas de BioArte y brindar información de la
+                Academia en las distintas especialidades de la Biotecnología.
+                Brinda la posibilidad de producir cruces que den lugar a grupos
+                de trabajos interdisciplinarios aunando la creatividad y
+                libertad del arte, el conocimiento académico y la tecnología de
+                la ciencia, para abordar los interrogantes que el futuro
+                presentará.
               </Desc>
             </Block>
           </DivIzq>
@@ -220,19 +223,21 @@ const IndexPage = () => {
               <Desc>
                 <br></br>
                 Proyecto Bios se estructura de la siguiente forma:<br></br>
-                <Bold>a) Site web con cinco áreas:</Bold>
-                Exhibición de BioArte- Panorama nacional y obras inéditas{" "}
                 <br></br>
-                Academia -Mapa Nacional de Universidades y sus
+                <Bold>a. Site web con cinco áreas: </Bold>
+                Exhibición de BioArte - Panorama nacional y obras inéditas{" "}
+                <br></br>
+                Academia - Mapa Nacional de Universidades y sus
                 especializaciones.
                 <br></br>
-                Biotecnología -Áreas de actividad, investigación y desarrollo
+                Biotecnología - Áreas de actividad, investigación y desarrollo
                 local.<br></br>
                 Acerca del BioArte - Hitos destacados , investigaciones y
-                aproximación al bioarte<br></br>
-                Espacio Biozen - un espacio de introspección y exploración
+                aproximación al bioarte.<br></br>
+                Soft Sanctuary - un espacio de introspección y exploración
                 sensorial <br></br>
-                <Bold>b) Agenda de Actividades:</Bold>
+                <br></br>
+                <Bold>b. Agenda de Actividades:</Bold>
                 Conversatorios, talleres, performances y master classes<br></br>
                 Acerca del BioArte: Timeline. Diferentes aproximaciones al
                 bioarte
@@ -249,19 +254,21 @@ const IndexPage = () => {
           <DivDer>
             <LightGuide flipY top="-90px" left="205px" />
             <Block>
-              <Title>¿Qué es Proyecto Bios?</Title>
+              <Title>¿Quiénes lo integran?</Title>
               <Desc>
                 <br></br>
-                Proyecto Bios pretende conformar un círculo virtuoso entre Arte,
-                Academia y Ciencia para acompañar, difundir y divulgar los
-                avances de la Biotecnología en el país, visibilizar y potenciar
-                los trabajos de artistas de BioArte y brindar información de la
-                propuesta de la Academia en las distintas especialidades de la
-                Biotecnología. Proyecto Bios brinda la posibilidad de producir
-                cruces que den lugar a grupos de trabajos interdisciplinarios
-                aunando la creatividad y libertad del arte, el conocimiento
-                académico y la tecnología de la ciencia, para abordar los
-                interrogantes que el futuro presentará.
+                <Bold>Producción y coordinación general: </Bold> Objeto a
+                <br></br>
+                <Bold>Diseño:</Bold> Valeria Scalise
+                <br></br>
+                <Bold>Experiencia web:</Bold> Iris Saladino (Programación) y
+                Laura Palavecino (Arte)
+                <br></br>
+                <Bold>Área Academia:</Bold> Guillermo Winnicki
+                <br></br>
+                <Bold>Área Bioarte: </Bold> Natalia Matewecki, Lucia Stubrin,
+                Objeto a<br></br>
+                <Bold>Soft Sanctuary: </Bold>Marie Claire Flanagan
               </Desc>
             </Block>
           </DivDer>
@@ -272,21 +279,13 @@ const IndexPage = () => {
               <Title>¿Cómo recorrer la experiencia?</Title>
               <Desc>
                 <br></br>
-                Al ingresar al sitio web encontrarán una recreación artística en
-                modelo 3D (realización L.Palavecino) del laboratorio original
-                del C3. Recorriendo el espacio con el mouse identificarán
-                objetos que representan las distintas áreas de Proyecto Bios.
-                Para ingresar a cada una de ellas, deberán cliquear en el objeto
-                correspondiente.
-                <br></br>
-                <br></br>
-                Durante los cinco días del evento habrá una agenda de charlas y
-                actividades que se transmitirán vía Zoom y por streaming del
-                Youtube del C3; habrá también talleres en Mozilla.Hubs y una
-                inauguración virtual de la exhibición en likelike. Para
-                participar deben inscribirse y recibirán las instrucciones y
-                respectivos links.<br></br> <br></br>¡Los invitamos a disfrutar
-                de la experiencia Proyecto Bios!
+                Al ingresar a la web encontrarán una recreación artística 360 en
+                modelo 3D del laboratorio original del C3. Recorriendo el
+                espacio identificarán objetos que representan las distintas
+                áreas de Proyecto Bios, que podrán clickear para ingresar. El
+                evento contempla una agenda virtual de charlas y actividades
+                durante cinco días. Para participar deben inscribirse dentro de
+                las secciones de Academia y Biotecnología.
               </Desc>
             </Block>
             <DivDer360>
